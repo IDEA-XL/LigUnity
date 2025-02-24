19d1e749956bdd08b8ec525724c4cec10eb206ba
BFeng14 <129063618+BFeng14@users.noreply.github.com>
2025-02-24 10:39:20 +0800

    Update run_al.sh

--- active_learning_scripts/run_al.sh
+++ active_learning_scripts/run_al.sh
@@ -1,16 +1,20 @@
 num_cycles=${1}
 begin_greedy=${2}
+weight_path1=${3}
+weight_path2=${4}
+result_path1=${5}
+result_path2=${6}
 
 python ./active_learning_scripts/run_cycle_ours.py \
     --input_file ../PARank_data_curation/case_study/tyk2_fep_label.csv \
-    --results_dir_1 "/cto_studio/xtalpi_lab/fengbin/PARank_save/FEP_pocket_epoch20_lr1/test_result/TYK2_FEP_AL_halfgreedy_fusion_pocket_debug" \
-    --results_dir_2 "/cto_studio/xtalpi_lab/fengbin/PARank_save/FEP_pocket_epoch20_lr1/test_result/TYK2_FEP_AL_halfgreedy_fusion_seq_debug" \
+    --results_dir_1 ${result_path1} \
+    --results_dir_2 ${result_path2} \
     --al_batch_size 100 \
     --num_cycles ${num_cycles} \
     --arch_1 pocket_ranking \
     --arch_2 protein_ranking \
-    --weight_path_1 "/cto_studio/xtalpi_lab/fengbin/PARank_save/FEP_pocket_epoch20_lr1/savedir_screen_con0.5_1/checkpoint_avg_41-50.pt" \
-    --weight_path_2 "/cto_studio/xtalpi_lab/fengbin/PARank_save/FEP_seq_epoch20_lr1/savedir_screen_con0.5_1/checkpoint_avg_41-50.pt" \
+    --weight_path_1 ${weight_path1} \
+    --weight_path_2 ${weight_path2} \
     --lr 0.0001 \
     --device 0 \
     --master_port 10071 \
